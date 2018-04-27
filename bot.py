b614ee33044fc1a5bb466a6bf10c44c9ca082b09
Aamhish Rao <aamhish@users.noreply.github.com>
2018-04-26 20:06:52 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -35,11 +35,11 @@ async def on_message(message):
         await client.send_message(message.channel, 'OI')
     elif(message.author.id=='338163785082601473'):
         print('Hayl send a message yay')
-        await client.change_nickname(338163785082601473, "Too AUSome")
+        await client.change_nickname(message.author, "Too AUSome")
     elif(message.author.id=='361549038958673924'):
         print('me send a message yay')
         print(message.author.id)
-        await client.change_nickname(361549038958673924, "Too AUSome")
+        await client.change_nickname(message.author, "Too AUSome")
     await client.process_commands(message)
 @client.async_event
 async def on_member_join(member):
